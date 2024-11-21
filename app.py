68db958b6fee7a1969df565ad9e0d5cdb892102a
suhelkhanca <suhel.hata@gmail.com>
2024-11-22 01:07:14 +0530

    add files

--- app.py
+++ app.py
@@ -1,79 +1,94 @@
-from flask import Flask, render_template, request, json
-from flask_mail import Mail
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import json
 from datetime import datetime
 
-local_server = True
+
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
+
+local_server = True
 app = Flask(__name__)
-
 app.config.update(
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME=params['gmail_username'],
-    MAIL_PASSWORD=params['gmail_password'],
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail_username'],
+    MAIL_PASSWORD=  params['gmail_password']
 )
-
 mail = Mail(app)
-
 if(local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(20), nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
     msg = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12))
-    
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
     def __repr__(self):
         return self.name
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
+
+    def __repr__(self):
+        return self.title
 
 @app.route("/")
 def home():
-    return render_template('index.html', params=params)
+    posts = Posts.query.filter_by().all()
+    return render_template('index.html', params=params, posts=posts)
 
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    posts = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
 
-@app.route("/contact", methods=['GET', 'POST'])
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
+    if(request.method=='POST'):
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-
-        entry = Contacts(name=name, phone_num=phone, date=datetime.now(), msg=message, email=email)
+        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
         db.session.add(entry)
         db.session.commit()
-        mail.send_message("New message from Blog",
+        mail.send_message('New message from ' + name,
                           sender=email,
-                          recipients=[params['gmail_username']],
-                          body=message + "\n" + phone
+                          recipients = [params['gmail-user']],
+                          body = message + "\n" + phone
                           )
     return render_template('contact.html', params=params)
-
-@app.route("/post")
-def post():
-    return render_template('post.html', params=params)
-
-@app.route("/contacts")
-def view_contacts():
-    all_contacts = Contacts.query.all()
-    return render_template('contacts.html', contacts=all_contacts, params=params)
+@app.route("/dashboards", methods = ['GET', 'POST'])
+def dashboards():
+    if request.method=='POST':
+        #Redirect to Admin panel
+        pass
+    else:
+        return render_template('login.html', params=params)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
